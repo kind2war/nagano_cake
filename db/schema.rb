deff909452497e0d8fa885df36b697dc047ae30c
kind2war <130664778+kind2war@users.noreply.github.com>
2023-07-14 13:56:14 +0000

    add devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_14_114757) do
+ActiveRecord::Schema.define(version: 2023_07_14_131642) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -22,10 +22,13 @@ ActiveRecord::Schema.define(version: 2023_07_14_114757) do
   end
 
   create_table "admins", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "encrypted_password", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "cart_items", force: :cascade do |t|
@@ -37,18 +40,20 @@ ActiveRecord::Schema.define(version: 2023_07_14_114757) do
   end
 
   create_table "customers", force: :cascade do |t|
-    t.string "family_name", null: false
-    t.string "first_name", null: false
-    t.string "rubi_family_name", null: false
-    t.string "rubi_first_name", null: false
-    t.string "post_code", null: false
-    t.string "address", null: false
-    t.string "phonenumber", null: false
-    t.string "email", null: false
-    t.string "encrypted_password", null: false
-    t.boolean "delete", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "family_name", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "rubi_family_name", default: "", null: false
+    t.string "rubi_first_name", default: "", null: false
+    t.string "post_code", default: "", null: false
+    t.string "address", default: "", null: false
+    t.string "phonenumber", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
   create_table "genres", force: :cascade do |t|
@@ -65,6 +70,19 @@ ActiveRecord::Schema.define(version: 2023_07_14_114757) do
     t.integer "sales_status", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "models", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "Customer"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_models_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
   end
 
   create_table "order_details", force: :cascade do |t|
